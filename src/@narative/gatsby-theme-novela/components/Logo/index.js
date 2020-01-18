b9a09955a380d2e461ec75d7af92ecf174708311
Borxes <yurasherman+github@gmail.com>
2020-01-18 17:50:27 +0200

    replace logo and add a couple articles

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Paste in your SVG logo and return it from this component.
+ * Make sure you have a height set for your logo.
+ * It is recommended to keep the height within 25-35px.
+ */
+export default function Logo() {
+  return (
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="240" height="240"><defs><path d="M55.99 37.24L209.96 37.24L209.96 191.2L55.99 191.2L55.99 37.24Z" id="c75VCzo0e1"></path></defs><g><g><g><use xlinkHref="#c75VCzo0e1" opacity="1" fill="#5d0d50" fill-opacity="1"></use></g></g></g></svg>
+  );
+}
